79ec30ef6e5d5f82ca721ed3247c60643aea5f3e
MDV209 <mitchelldevos@outlook.com>
2025-01-24 22:57:39 -0800

    Added customer walking in functionality

--- Scene/at_ease.gd
+++ Scene/at_ease.gd
@@ -1,11 +1,21 @@
 extends Sprite2D
 
-
+@export var speed = 0.25
+var spawned = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	hide()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("spawnCustomer"):
+		spawnCustomer()
+
+	if position.x <= 575 && spawned:
+		position.x += delta * speed
+
+func spawnCustomer() -> void:
+	show()
+	spawned = true
+	
