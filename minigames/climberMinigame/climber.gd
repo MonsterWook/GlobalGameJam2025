45493d5e78febd3d66e3486024ca390f7b45c3ef
WookMG <martinaranogabelsberger@gmail.com>
2025-01-26 11:52:04 -0800

    climbingMinigame

--- minigames/climberMinigame/climber.gd
+++ minigames/climberMinigame/climber.gd
@@ -16,7 +16,6 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jumpStrength * -1
