a3f1fc0e246c5a3bf1da9157a0c73b74f3793dd0
WookMG <martinaranogabelsberger@gmail.com>
2025-01-26 12:40:58 -0800

    fixed and art

--- minigames/climberMinigame/climberMinigame.gd
+++ minigames/climberMinigame/climberMinigame.gd
@@ -4,12 +4,14 @@ signal changeSceneWin
 signal changeSceneLose
 
 func win(body):
-	print(":)")
-	await get_tree().create_timer(1).timeout
-	changeSceneWin.emit()
+	if body.is_in_group("player"):
+		print(":)")
+		await get_tree().create_timer(1).timeout
+		changeSceneWin.emit()
 
 
 func lose(body):
-	print(":(")
-	await get_tree().create_timer(1).timeout
-	changeSceneLose.emit()
+	if body.is_in_group("player"):
+		print(":(")
+		await get_tree().create_timer(1).timeout
+		changeSceneLose.emit()
