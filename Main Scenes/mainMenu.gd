d8ada073f4d40d930b648dd1a3664fde4fe8db26
WookMG <martinaranogabelsberger@gmail.com>
2025-01-26 16:16:15 -0800

    mainmenu

--- Main Scenes/mainMenu.gd
+++ Main Scenes/mainMenu.gd
@@ -2,19 +2,21 @@ extends Node2D
 
 @onready var anim = $AnimationPlayer
 @onready var va = $VA
-var nextText = 0
-var intro: Array[String] = ["I’ve always had the ability to read people's thought bubbles", \
- "from the day I was born.", \
- "I dreamt I would grow up and become a superhero", \
- "that I would use my gift to help others.", \
- "Unfortunately I have bills to pay and the money isn’t going to make itself", \
- "so I’ll use my powers to tell people what they want to hear", \
- "because let’s be honest who wants to hear the truth."]
+@onready var vaAudio = $AudioStreamPlayer2D
 
+var next = 0
+var intro: Array[String] = ["I’ve always had the ability to read people's thought bubbles", \
+ "From the day I was born", \
+ "I dreamt I would grow up and become a superhero", \
+ "That I would use my gift to help others", \
+ "Unfortunately, I have bills to pay and the money isn’t going to make itself", \
+ "So I’ll use my powers to tell people what they want to hear", \
+ "because, let’s be honest, who wants to hear the truth."]
+
+@export var va_audio: Array[AudioStream] = []
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	anim.play("intro")
-	
 func _process(delta):
 	if Input.is_action_just_pressed("Skip"):
 		get_tree().change_scene_to_file("res://Main Scenes/office.tscn")
@@ -23,5 +25,7 @@ func startGame():
 	get_tree().change_scene_to_file("res://Main Scenes/office.tscn")
 
 func progressText():
-	va.text = intro[nextText]
-	nextText += 1
+	vaAudio.stream = va_audio[next]
+	vaAudio.play()
+	va.text = intro[next]
+	next += 1
